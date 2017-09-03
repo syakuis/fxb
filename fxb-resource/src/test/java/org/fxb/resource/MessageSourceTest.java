6c7bcbcf2ead1a2a432c8177fe9a9a5463eaf2c0
syaku <syaku@naver.com>
2017-09-03 17:24:49 +0900

    fxb-context 추가 - 다양한 라이브러리를 지원할 예정. 해야할일들 정리...

---
+++ fxb-resource/src/test/java/org/fxb/resource/MessageSourceTest.java
@@ -0,0 +1,19 @@
+package org.fxb.resource;
+
+/**
+ * @author Seok Kyun. Choi. 최석균 (Syaku)
+ * @site http://syaku.tistory.com
+ * @since 2017. 9. 3.
+ */
+public class MessageSourceTest {
+
+	// todo Spring ReloadableResourceBundleMessageSource 를 이용하여 로드하기
+
+	// todo Fxb MessageSourceFactoryBean 으로 로드하기
+
+	// todo 운영 환경에 따른 로드 테스트
+
+	// todo 로드 순서에 따른 Overwrite 잘 되었는 지
+
+	// todo 위 두가지 비교하기
+}
---
+++ fxb-web/src/test/java/org/fxb/boot/BasicContextConfigurationTest.java
@@ -0,0 +1,17 @@
+package org.fxb.boot;
+
+/**
+ * @author Seok Kyun. Choi. 최석균 (Syaku)
+ * @site http://syaku.tistory.com
+ * @since 2017. 9. 3.
+ */
+public class BasicContextConfigurationTest {
+
+	// todo Properties Test
+
+	// todo Config Test
+
+	// todo 그외 기본 설정이 잘 구성되었는 지 확인한다. 절대경로
+
+	// todo MessageSource OverWrite Test
+}
